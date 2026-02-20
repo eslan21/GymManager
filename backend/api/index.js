95d0335501c639109450fc8d1a52457b038359a3
Freddy Torrealba <freddyabraham91@gmail.com>
2026-02-20 20:01:26 +0100

    try 6

--- backend/api/index.js
+++ backend/api/index.js
@@ -42,8 +42,13 @@ module.exports = async (req, res) => {
     await connectDB();
 
     // Configuración de CORS
+    const origin = req.headers.origin;
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (origin) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
